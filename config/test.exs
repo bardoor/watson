84a7eec7cff3b4607cbf5c4b3846dbd0bd85494e
bardoor <dmitrybartenev58@gmail.com>
2026-06-30 20:49:53 +0300

    Remove old version

--- config/test.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :watson,
-  capture_helper: Watson.TestCaptureHelper,
-  mlx_whisper_path: Path.expand("../test/support/mlx_whisper_stub", __DIR__),
-  transcription_cadence_ms: 60_000
